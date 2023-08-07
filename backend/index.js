13877817e759ead7a6525d36a8a4b99baede4d56
Suvin <suvinkrsingh@gmail.com>
2023-08-08 01:35:13 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 const port = 5000
 const mongoDB=require("./db")
 
 
-app.use(cors());
+// app.use(cors());
 mongoDB();  
 //middleware
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","https://www.frizzlefeast.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));
